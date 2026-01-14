44c734e971da8fe2e2bdd4e773a9d1adc43b12ab
Martin <revoxgfx@gmail.com>
2026-01-14 17:06:25 +0100

    installed shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ import Tab3 from './pages/Tab3';
 import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
+// import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,14 @@
-/// <reference types="vitest" />
-
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
+  plugins: [react(), legacy(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
